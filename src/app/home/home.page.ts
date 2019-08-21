97eff80470fc4b35296fcb1793cadcd79705b537
Rik de Vos <rik@rikdevos.com>
2019-08-21 08:39:33 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  constructor(private iab: InAppBrowser) {}
 
-  constructor() {}
-
+  openBrowserBtn() {
+    const browser = this.iab.create('https://ionicframework.com/', '_blank');
+  }
 }
